717261178e8270742e4ddc35ea63eb7d4c2cfc04
eudireto2 <Robert Garcia>
2022-08-23 12:08:34 -0400

    chore: change comment

--- nodejs/desafio-use-process/gettings.js
+++ nodejs/desafio-use-process/gettings.js
@@ -2,4 +2,4 @@ const getFlag = require('./flag');
 
 console.log(`Oi ${getFlag('--name')}. ${getFlag('--getting')}`);
 
-//node gettings --name "Robert Garcia" --getting "Como você está?" -> utilizar no terminal
+//node gettings --name "Robert Garcia" --getting "Como vai a vida?" -> utilizar no terminal
