27e1fba54a0a43dda0cdcbbb460c7fe64595b12e
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-08-24 18:58:34 -0400

    feat(bestme-app): read answers to questions

--- nodejs/bestme-app/index.js
+++ nodejs/bestme-app/index.js
@@ -6,12 +6,18 @@ const questions = [
 ];
 
 const ask = (index = 0) => {
-  process.stdout.write(questions[index] + "\n\n");
+  process.stdout.write("\n" + questions[index] + " > ");
 }
 
 ask();
 
+const answers = [];
 process.stdin.on("data", data => {
-  process.stdin.write( data.toString().trim() + "\n");
-  process.exit();
+  answers.push(data.toString().trim());
+  if(answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    console.log(answers);
+    process.exit();
+  }
 });
